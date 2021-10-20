1fd1bc741e62f063ce7d88be60b8dccbae17837e
Amey Kasbe <ameykasbe@Ameys-MacBook-Air.local>
2021-10-19 20:35:00 -0500

    Distribution of message types with no time duration

--- src/main/scala/Execution.scala
+++ src/main/scala/Execution.scala
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.apache.hadoop.mapreduce.{Job, Mapper, Reducer}
 import scala.collection.JavaConverters._
-
+import com.typesafe.config._
 import mappers._
 import reducers._
 
@@ -18,19 +18,45 @@ object Execution{
   def main(args: Array[String]): Unit = {
     val configuration = new Configuration
     configuration.set("mapred.textoutputformat.separator", ",")
-    val distributionJob = Job.getInstance(configuration,"Distribution message types")
-    distributionJob.setJarByClass(this.getClass)
-    // Mapper config
-    distributionJob.setMapperClass(classOf[DistributionMapper])
-    distributionJob.setCombinerClass(classOf[DistributionReducer])
-    // Reducer config
-    distributionJob.setReducerClass(classOf[DistributionReducer])
-    distributionJob.setOutputKeyClass(classOf[Text])
-    distributionJob.setOutputKeyClass(classOf[Text]);
-    distributionJob.setOutputValueClass(classOf[IntWritable]);
-    FileInputFormat.addInputPath(distributionJob, new Path(args(0)))
-    FileOutputFormat.setOutputPath(distributionJob, new Path(args(1)))
-    println("Starting MapReduce job...")
-    System.exit(if(distributionJob.waitForCompletion(true))  0 else 1)
+    val config = ConfigFactory.load()
+
+    if (args(0) == "1") {
+      val distributionJob = Job.getInstance(configuration,"Distribution message types limited by time duration")
+      distributionJob.setJarByClass(this.getClass)
+      // Mapper config
+      distributionJob.setMapperClass(classOf[DistributionMapper])
+      distributionJob.setCombinerClass(classOf[DistributionReducer])
+      // Reducer config
+      distributionJob.setReducerClass(classOf[DistributionReducer])
+      distributionJob.setOutputKeyClass(classOf[Text])
+      distributionJob.setOutputKeyClass(classOf[Text]);
+      distributionJob.setOutputValueClass(classOf[IntWritable]);
+      FileInputFormat.addInputPath(distributionJob, new Path(args(1)))
+      val outputPath = config.getString("OutputLocation.MainLocation") + config.getString("OutputLocation.DistributionMessageType")
+      FileOutputFormat.setOutputPath(distributionJob, new Path(outputPath))
+      println("Starting MapReduce job...")
+      System.exit(if(distributionJob.waitForCompletion(true))  0 else 1)
+    }
+
+    if (args(0) == "3") {
+      val distributionJob = Job.getInstance(configuration,"Distribution message types NOT limited by time duration")
+      distributionJob.setJarByClass(this.getClass)
+      // Mapper config
+      distributionJob.setMapperClass(classOf[DistributionMapperNoTime])
+      distributionJob.setCombinerClass(classOf[DistributionReducerNoTime])
+      // Reducer config
+      distributionJob.setReducerClass(classOf[DistributionReducerNoTime])
+      distributionJob.setOutputKeyClass(classOf[Text])
+      distributionJob.setOutputKeyClass(classOf[Text]);
+      distributionJob.setOutputValueClass(classOf[IntWritable]);
+      FileInputFormat.addInputPath(distributionJob, new Path(args(1)))
+      val outputPath = config.getString("OutputLocation.MainLocation") + config.getString("OutputLocation.DistributionMessageTypeNoTime")
+      FileOutputFormat.setOutputPath(distributionJob, new Path(outputPath))
+      println("Starting MapReduce job...")
+      System.exit(if(distributionJob.waitForCompletion(true))  0 else 1)
+    }
+
+
+
   }
 }
---
+++ src/main/scala/reducers/DistributionReducerNoTime.scala
@@ -0,0 +1,14 @@
+package reducers
+
+import org.apache.hadoop.io.{IntWritable, Text}
+import org.apache.hadoop.mapreduce.Reducer
+import java.lang.Iterable
+import scala.collection.JavaConverters._
+
+class DistributionReducerNoTime extends Reducer[Text,IntWritable,Text,IntWritable] {
+  override def reduce(key: Text, values: Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = {
+    var sum = values.asScala.foldLeft(0)(_ + _.get)
+    context.write(key, new IntWritable(sum))
+  }
+
+}
